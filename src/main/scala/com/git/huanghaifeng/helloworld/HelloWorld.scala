d05bb4a0a47a4472835ec3b655d42217643af243
haifeng.huang <1756983926@qq.com>
2017-06-29 14:33:25 +0800

    add files

--- src/main/scala/com/git/huanghaifeng/helloworld/HelloWorld.scala
+++ src/main/scala/com/git/huanghaifeng/helloworld/HelloWorld.scala
@@ -1,7 +1,24 @@
 package com.git.huanghaifeng.helloworld
 
 object HelloWorld {
-    def main(args: Array[String]){
-        println("haha, hello, I'am coming")
-    }
+//    def main(args: Array[String]){
+//        println("haha, hello, I'am coming")
+//    }
+  
+    def main(args: Array[String]) {
+  
+        val x = HelloWorld(5)
+        println(x)
+  
+        x match
+        {
+           case HelloWorld(num) => println(x + " 是 " + num + " 的两倍！")
+           //unapply 被调用
+           case _ => println("无法计算")
+        }
+  
+     }
+     def apply(x: Int) = x*2
+     def unapply(z: Int): Option[Int] = if (z%2==0) Some(z/2) else None
 }
+
